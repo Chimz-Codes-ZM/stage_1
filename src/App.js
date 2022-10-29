88502d742ee23d799fe2811b5f11c3d15786733e
Chimwemwe Masona <chimmasona@gmail.com>
2022-10-29 13:09:13 +0200

    These are the first changes to this task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './Components/Profile_section';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='main_section'>
+      <Profile/>
+      </div> 
+
+      <div className='social_section'>
+        <h1>Social Section</h1>
+      </div>
+      <div className='footer'>footer</div>
     </div>
   );
 }
---
+++ src/Components/Profile_section.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Profile() {
+    return (
+        <div className="profile">
+            <img src="./Images/IMG_20221027_082022.jpg" alt="profile picture" id="profile__img"/>
+            <h4>Chimwemwe Masona</h4>
+        </div>
+    )
+}
+
+export default Profile
